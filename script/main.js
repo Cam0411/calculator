9cbbe82cf7a4771573f533f57cc07d19e2ddc452
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2022-02-08 20:05:00 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const ans__1 = document.querySelector(".ans__1")
+const ans__2 = document.querySelector(".ans__2")
+const ans__3 = document.querySelector(".ans__3")
+const ans__4 = document.querySelector(".ans__4")
+
+ans__1.addEventListener("click", () => {
+    console.log(false);
+})
